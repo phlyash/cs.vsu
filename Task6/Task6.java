c03942c0e7d3017ee40058a66c97b8d4a030e396
Philipp <72046147+phlyne@users.noreply.github.com>
2022-09-29 18:23:53 +0300

    Update Task6.java

--- Task6/Task6.java
+++ Task6/Task6.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 
 class Task6 {
+
     // #3
     // a(n - 1) * x * x
     // a(n-1) * x * x * (2 * n - 3) / (2 * n - 1)
+
     public static void main(String[] args){
+        double x, e;
+        int n;
+
         Scanner scanner = new Scanner(System.in);
-        double x = scanner.nextDouble(), e = scanner.nextDouble();
-        int n = scanner.nextInt();
+        System.out.print("Введите x - ");
+        x = scanner.nextDouble();
+        System.out.print("Введите e - ");
+        e = scanner.nextDouble();
+        System.out.print("Введите n - ");
+        n = scanner.nextInt();
+
         double a = 2 * x;
         double sumN = 0, sumE = 0, sumEDiv10 = 0;
         int i = 1;
@@ -18,9 +28,10 @@ class Task6 {
             i++;
             a *= Math.pow(x, 2) * (2 * i - 3) / (2 * i - 1);
         }
-        System.out.println(sumN);
-        System.out.println(sumE);
-        System.out.println(sumEDiv10);
-        System.out.println(Math.log((1 + x) / (1 - x)));
+
+        System.out.printf("Сумма первых n чисел - %.15f\n", sumN);
+        System.out.printf("Сумма чисел меньших e по абс. - %.15f\n", sumE);
+        System.out.printf("Сумма чисел меньших e/10 по абс. - %.15f\n", sumEDiv10);
+        System.out.printf("Значение при помощи функции в Math - %.15f\n", Math.log((1 + x) / (1 - x)));
     }
 }
