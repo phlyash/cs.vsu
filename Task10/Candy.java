9b919eedc06d718be0d7b585c3ecc8fdc3a61ddd
Philipp <72046147+phlyne@users.noreply.github.com>
2022-11-12 00:24:15 +0300

    Create Candy.java

---
+++ Task10/Candy.java
@@ -0,0 +1,12 @@
+public class Candy {
+    public int price;
+    public String name;
+    public Candy(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+    public Candy(String name, int price) {
+        this.price = price;
+        this.name = name;
+    }
+}
